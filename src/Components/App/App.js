7581ca3a2d93cf5fdcb3425f762b38dce2c1395f
mtan014 <mtan014@e.ntu.edu.sg>
2019-12-05 12:57:41 +0800

    Moved App into component folder

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { BenchmarkingChart } from "../BenchmarkingChart/BenchmarkingChart";
+
+function App() {
+  return (
+    <div className="App">
+      <BenchmarkingChart />
+    </div>
+  );
+}
+
+export default App;
