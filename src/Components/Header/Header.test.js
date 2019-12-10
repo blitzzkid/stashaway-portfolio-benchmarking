6275c173b6da27ba8393aa2028625eb3f58ff559
mtan014 <mtan014@e.ntu.edu.sg>
2019-12-10 22:44:54 +0800

    Add tests for header

---
+++ src/Components/Header/Header.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { Header } from "./Header";
+
+describe("It renders the header correctly", () => {
+  it("Shows the menu link 'Home'", () => {
+    const { getByText } = render(<Header />);
+    expect(getByText("Home")).toBeInTheDocument();
+  });
+
+  it("Shows the menu link 'Manage deposits'", () => {
+    const { getByText } = render(<Header />);
+    expect(getByText("Manage deposits")).toBeInTheDocument();
+  });
+
+  it("Shows the menu link 'Refer a friend'", () => {
+    const { getByText } = render(<Header />);
+    expect(getByText("Refer a friend")).toBeInTheDocument();
+  });
+
+  it("Shows the menu link 'Support'", () => {
+    const { getByText } = render(<Header />);
+    expect(getByText("Support")).toBeInTheDocument();
+  });
+
+  it("Shows the menu link 'Oliver'", () => {
+    const { getByText } = render(<Header />);
+    expect(getByText("Oliver")).toBeInTheDocument();
+  });
+});
