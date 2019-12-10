6bf69e754857047ad589406bc4e2b7b62db11e12
mtan014 <mtan014@e.ntu.edu.sg>
2019-12-10 19:19:02 +0800

    Add CSS for benchmark selection

--- src/Components/PortfolioBenchmark/BenchmarkSelection/BenchmarkSelection.js
+++ src/Components/PortfolioBenchmark/BenchmarkSelection/BenchmarkSelection.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./BenchmarkSelection.css";
 
 export class BenchmarkSelection extends React.Component {
   onBenchmarkPortfolioChange = event => {
@@ -8,28 +9,30 @@ export class BenchmarkSelection extends React.Component {
 
   render() {
     return (
-      <div>
-        <div>
+      <div className="benchmarkSelection__container">
+        <div className="benchmarkSelection__label">
           <p>General Investing</p>
-          <p>StashAway Risk Index 14%</p>
+          <span>StashAway Risk Index 14%</span>
         </div>
-        <p>vs</p>
-        <section>
-          <select
-            id="benchmarkPorfolio"
-            name="benchmarkPortfolio"
-            onChange={this.onBenchmarkPortfolioChange}
-            type="text"
-          >
-            <option value="">Which benchmark do you want to compare?</option>
-            <option value="vanguard4060">
-              40% VTSMX (Stock) + 60% VBMFX (Bond)
-            </option>
-            <option value="ishares2080">
-              20% IVV (Stock) + 80% GOVT (Bond)
-            </option>
-          </select>
-        </section>
+        <span className="benchmarkSelection__vs">vs</span>
+        <div className="benchmarkSelection__dropdown">
+          <div className="benchmarkSelection__dropdownBox">
+            <select
+              id="benchmarkPorfolio"
+              name="benchmarkPortfolio"
+              onChange={this.onBenchmarkPortfolioChange}
+            >
+              <option value="">Which benchmark do you want to compare?</option>
+              <option value="vanguard4060">
+                40% VTSMX (Stock) + 60% VBMFX (Bond)
+              </option>
+              <option value="ishares2080">
+                20% IVV (Stock) + 80% GOVT (Bond)
+              </option>
+            </select>
+            <div className="dropdown__arrow"></div>
+          </div>
+        </div>
       </div>
     );
   }
