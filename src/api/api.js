98a573a29d21bb18f3d57e402253e277c8832828
mtan014 <mtan014@e.ntu.edu.sg>
2019-12-11 15:27:42 +0800

    Update functions for api calls

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,18 @@
 import axios from "./axios";
 
-export const fetchMsciWorldEtf = async () => {
-  const ticker = "URTH";
+export const fetchHistoricalEtfDataOfTicker = async ticker => {
   const response = await axios.get(
-    `/history?symbol=${ticker}&sort=newest&api_token=${process.env.REACT_APP_MSCI_ETF_TICKER_TOKEN}`,
+    `/history?symbol=${ticker}&sort=newest&api_token=${process.env.REACT_APP_TRADING_DATA_TOKEN}`
+  );
+  return response.data;
+};
+
+export const fetchHistoricalCurrencyExchangeRateData = async (
+  baseCurrency,
+  targetCurrency
+) => {
+  const response = await axios.get(
+    `/forex_history?base=${baseCurrency}&convert_to=${targetCurrency}&sort=newest&api_token=${process.env.REACT_APP_TRADING_DATA_TOKEN}`
   );
   return response.data;
 };
